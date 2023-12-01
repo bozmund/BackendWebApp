abbf7d46f6f2ccffd0b193eddff6f83d6eb756c8
bozmund <eboac12070@gmail.com>
2023-12-01 08:14:00 +0100

    SpotifyToken Working

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-import express from 'express'
-const port = 8080
-const app = express()
-const router = express.Router()
+import express from "express";
+import {} from "dotenv/config";
+import { _getToken } from "./spotifyconnector/_getToken.js";
 
-app.use(express.json())
-app.use('/api',router)
+const port = 3000;
+const app = express();
+const router = express.Router();
+const client_credentials = {
+  client_id: process.env.SPOTIFY_CLIENT_ID,
+  client_secret: process.env.SPOTIFY_CLIENT_SECRET,
+};
+const access_token = await _getToken(
+  client_credentials.client_id,
+  client_credentials.client_secret
+);
+if (!access_token) {
+  console.log("access_token not found");
+  process.exit(1);
+}
+
+app.use(express.json());
+app.use("/api", router);
+
+//app.route("/").get(async (req, res) => {
+//  const response = await _getToken(
+//    client_credentials.client_id,
+//    client_credentials.client_secret
+//  );
+//  res.send(response);
+//});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
---
+++ spotifyconnector/_getToken.js
@@ -0,0 +1,17 @@
+async function _getToken(spotifyClientID, spotifyClientSecret) {
+  const result = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization:
+        "Basic " + btoa(spotifyClientID + ":" + spotifyClientSecret),
+    },
+    body: "grant_type=client_credentials",
+  });
+
+  const data = await result.json();
+  const access_token = data.access_token;
+  return access_token;
+}
+
+export { _getToken };
