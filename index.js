74921faab45a0e25e99ddb18fbe2af5792be505a
bozmund <eboac12070@gmail.com>
2023-12-02 18:37:45 +0100

    added connection to mongodb

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 import express from "express";
+import mongoose from "mongoose";
 import {} from "dotenv/config";
 import { _getSpotifyToken } from "./spotifyconnector/_getSpotifyToken.js";
 import { _getTidalToken } from "./tidalconnector/_getTidalToken.js";
 
-const port = 3000;
-const app = express();
-const router = express.Router();
 const client_credentials = {
   spotify_client_id: process.env.SPOTIFY_CLIENT_ID,
   spotify_client_secret: process.env.SPOTIFY_CLIENT_SECRET,
   tidal_client_id: process.env.TIDAL_CLIENT_ID,
   tidal_client_secret: process.env.TIDAL_CLIENT_SECRET,
 };
+
+const port = 3000;
+const app = express();
+const router = express.Router();
+
+app.use(express.json());
+app.use("/api", router);
+
+mongoose.connect(process.env.MONGODB_URI);
+
 const spotify_access_token = await _getSpotifyToken(
   client_credentials.spotify_client_id,
   client_credentials.spotify_client_secret
@@ -20,16 +28,11 @@ const tidal_access_token = await _getTidalToken(
   client_credentials.tidal_client_id,
   client_credentials.tidal_client_secret
 );
-console.log(spotify_access_token);
-console.log(tidal_access_token);
 
-if (!spotify_access_token) {
+if (!spotify_access_token || !tidal_access_token) {
   console.log("access_token not found");
   process.exit(1);
 }
-
-app.use(express.json());
-app.use("/api", router);
 
 //app.route("/").get(async (req, res) => {
 //  const response = await _getToken(
