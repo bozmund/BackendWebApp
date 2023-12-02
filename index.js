bac43ea80e3b4cc2dbaee493276ec2963a4f6600
bozmund <eboac12070@gmail.com>
2023-12-02 17:44:11 +0100

    Added Tidal Connection

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 import express from "express";
 import {} from "dotenv/config";
-import { _getToken } from "./spotifyconnector/_getToken.js";
+import { _getSpotifyToken } from "./spotifyconnector/_getSpotifyToken.js";
+import { _getTidalToken } from "./tidalconnector/_getTidalToken.js";
 
 const port = 3000;
 const app = express();
 const router = express.Router();
 const client_credentials = {
-  client_id: process.env.SPOTIFY_CLIENT_ID,
-  client_secret: process.env.SPOTIFY_CLIENT_SECRET,
+  spotify_client_id: process.env.SPOTIFY_CLIENT_ID,
+  spotify_client_secret: process.env.SPOTIFY_CLIENT_SECRET,
+  tidal_client_id: process.env.TIDAL_CLIENT_ID,
+  tidal_client_secret: process.env.TIDAL_CLIENT_SECRET,
 };
-const access_token = await _getToken(
-  client_credentials.client_id,
-  client_credentials.client_secret
+const spotify_access_token = await _getSpotifyToken(
+  client_credentials.spotify_client_id,
+  client_credentials.spotify_client_secret
 );
-if (!access_token) {
+const tidal_access_token = await _getTidalToken(
+  client_credentials.tidal_client_id,
+  client_credentials.tidal_client_secret
+);
+console.log(spotify_access_token);
+console.log(tidal_access_token);
+
+if (!spotify_access_token) {
   console.log("access_token not found");
   process.exit(1);
 }
